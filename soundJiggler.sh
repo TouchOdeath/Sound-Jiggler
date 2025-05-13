28e8da35f76904af9d029e1c7f559c293716270c
TouchOdeath <5149109+TouchOdeath@users.noreply.github.com>
2025-05-12 21:51:45 -0500

    Update soundJiggler.sh

--- soundJiggler.sh
+++ soundJiggler.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-JiggleEvery=$(yad --scale --text="Jiggle every X seconds" --min-value=1 --max-value=60 --value=30 --title="" --center)
+JiggleEvery=$(yad --form --field="Jiggle every (seconds):num" "60!1..300!1" --title="Sound Jiggler" --width=300 --center --button="OK:0" --button="Cancel:1" | cut -d'|' -f1)
 
 if [ $? -ne 0 ]; then
   echo "Exiting..."
   exit
 fi
 
-(
-  while true; do
-    paplay --volume=1 /usr/share/sounds/alsa/Front_Center.wav
 
-    for ((i = 0; i < JiggleEvery; i++)); do
-      echo $(( (i * 100) / JiggleEvery ))
-      sleep 1
-    done
-  done
-) | yad --progress --pulsate --auto-close --title="Sound Jiggling Every $JiggleEvery Seconds" --text="" --center --width=500 --button="Exit:0"
+if [ $? -eq 0 ]; then
+	(
+	  while true; do
+		paplay --volume=1 /usr/share/sounds/alsa/Front_Center.wav
+
+		for ((i = 0; i < JiggleEvery; i++)); do
+		  echo $(( (i * 100) / JiggleEvery ))
+		  sleep 1
+		done
+	  done
+	) | yad --progress --pulsate --auto-close --title="Sound Jiggling Every $JiggleEvery Seconds" --center --width=500 --button="Exit:0"
+fi
